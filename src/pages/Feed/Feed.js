3c02498bbab8ec3e8ae163bcc5100a010b89c49e
Essien Emmanuel <essienemma300@gmail.com>
2023-02-28 23:04:45 +0100

    fixed the updated total posts bug

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -275,12 +275,14 @@ class Feed extends Component {
                 };
                 this.setState((prevState) => {
                     let updatedPosts = [...prevState.posts];
+                    let updatedTotalPosts = prevState.totalPosts;
                     if (prevState.editPost) {
                         const postIndex = prevState.posts.findIndex(
                             (p) => p._id === prevState.editPost._id,
                         );
                         updatedPosts[postIndex] = post;
                     } else {
+                        updatedTotalPosts++;
                         updatedPosts.pop();
                         updatedPosts.unshift(post);
                     }
@@ -289,6 +291,7 @@ class Feed extends Component {
                         isEditing: false,
                         editPost: null,
                         editLoading: false,
+                        totalPosts: updatedTotalPosts,
                     };
                 });
             })
